c3599056bc56fb51e682cbd47ac5b8adde9e9118
Dav-Zuko <108547729+Dav-Zuko@users.noreply.github.com>
2026-07-01 22:01:04 -0500

    update: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Playfair_Display } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
