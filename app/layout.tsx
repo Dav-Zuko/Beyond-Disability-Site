bb90cb5bcd7db7f2c2bba828b2829b0b6a885325
Dav-Zuko <108547729+Dav-Zuko@users.noreply.github.com>
2026-07-01 22:14:13 -0500

    update: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
